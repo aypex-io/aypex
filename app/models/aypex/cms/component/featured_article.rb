ecc304c8065c9524504e72403235088267bd15a7
Matthew Kennedy <m.kennedy@me.com>
2023-05-05 11:54:51 +0100

    Remove linked resources from article.

--- app/models/aypex/cms/component/featured_article.rb
+++ app/models/aypex/cms/component/featured_article.rb
@@ -1,10 +1,4 @@
 module Aypex::Cms::Component
   class FeaturedArticle < Aypex::CmsComponent
-    LINKED_RESOURCE_TYPES = ["Aypex::Category", "Aypex::Product", "Aypex::CmsPage"]
-
-    typed_store :settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
-      s.string :title
-      s.string :button_text
-    end
   end
 end
