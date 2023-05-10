69fd3e3bb7bb4510ca3208693c06326c8f6d35b6
Matthew Kennedy <m.kennedy@me.com>
2023-05-10 10:50:59 +0100

    Fix cms nested attributes.

--- app/models/aypex/cms/component/image_hero.rb
+++ app/models/aypex/cms/component/image_hero.rb
@@ -3,8 +3,9 @@ module Aypex::Cms::Component
     LINKED_RESOURCE_TYPES = ["Aypex::Category", "Aypex::Product", "Aypex::CmsPage"]
 
     typed_store :settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
-      s.string :title
-      s.string :button_text
+      s.string :headline, default: "Default Headline", null: false
+      s.string :subtitle, default: "", null: false
+      s.string :button_text, default: "", null: false
     end
   end
 end
